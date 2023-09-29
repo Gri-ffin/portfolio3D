2dd9ebb163a335524768bec77f07edc2e79b291c
Gri-ffin <gameshunter359@gmail.com>
2023-09-29 20:27:59 +0100

    fix styling bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components/'
 
