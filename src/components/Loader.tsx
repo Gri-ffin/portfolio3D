e79a1a04ba9154fe8cb9525dba4b40248c4adc70
Gri-ffin <gameshunter359@gmail.com>
2023-10-05 20:16:55 +0100

    add the loader component

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,6 +1,17 @@
+import { Html, useProgress } from '@react-three/drei'
+
 const Loader = () => {
+  const { progress } = useProgress()
+
   return (
-    <div>Loader</div>
+    <Html>
+      <span className='canvas-loader'>
+        <p className='text-[14px] text-[#f1f1f1] font-extrabold mt-[40px]'
+        >
+          {progress.toFixed(2)}%
+        </p>
+      </span>
+    </Html>
   )
 }
 
