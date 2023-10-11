f37dc9971f463c69914fc8fd29818d3f88fefc98
Gri-ffin <gameshunter359@gmail.com>
2023-10-11 20:33:36 +0100

    finish about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,52 @@
-const Contact = () => {
+import { Tilt } from "react-tilt"
+import { motion } from "framer-motion"
+import { styles } from "../styles"
+import { services } from "../constants"
+import { fadeIn } from "../utils/motion"
+import { SectionWrapper } from "../hoc"
+
+const ServiceCard = ({ index, title, icon }: { index: number, title: string, icon: string }) => {
   return (
-    <div>Contact</div>
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-md shadow-card"
+      >
+        <div
+          className="bg-tertiary rounded-md py-5 px-12 min-h-[250px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16  h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>)
+}
+
+const About = () => {
+  return (
+    <SectionWrapper idName="about">
+      <motion.div>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
+
+      <motion.p
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        variants={fadeIn("up", "", 0.1, 1)}
+      >
+        A passionate and fast learner Full Stack Software Developer
+        having experience in building Web application with
+        Typescript / ReactJs / NodeJs / AngularJs and some
+        other cool libraries and frameworks.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </SectionWrapper>
   )
 }
 
-export default Contact
+export default About
