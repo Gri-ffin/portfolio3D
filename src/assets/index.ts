4d2d3e9e5992f73baf06f466be2dc406b4ca5af7
Gri-ffin <gameshunter359@gmail.com>
2023-10-18 19:32:30 +0100

    add tech image and imports

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -8,18 +8,16 @@ import menu from "./menu.svg";
 import close from "./close.svg";
 
 import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
 import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
+import python from './tech/python.png';
+import next from './tech/next.png';
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
@@ -40,18 +38,16 @@ export {
   menu,
   close,
   css,
-  docker,
-  figma,
   git,
   html,
   javascript,
   mongodb,
   nodejs,
   reactjs,
-  redux,
   tailwind,
   typescript,
-  threejs,
+  python,
+  next,
   meta,
   shopify,
   starbucks,
