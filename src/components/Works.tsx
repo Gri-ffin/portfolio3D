2271f3b9013c95e254c68844b9285e85e4a07cce
Gri-ffin <gameshunter359@gmail.com>
2023-10-20 19:52:25 +0100

    finish work component

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,7 +1,92 @@
+import { github } from "../assets"
+import { SectionWrapper } from "../hoc"
+import { projects } from "../constants"
+import { fadeIn, textVariant } from "../utils/motion"
+import { motion } from "framer-motion"
+import { styles } from "../styles"
+import { Tilt } from "react-tilt"
+
 const Works = () => {
   return (
-    <div>Works</div>
+    <SectionWrapper idName="">
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My projects!</p>
+        <h2 className={styles.sectionHeadText}>Projects</h2>
+      </motion.div>
+
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn('left', '', 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          Following projects showcases my skills and experience through
+          real-world examples of my work. Each project is briefly described with
+          links to code repositories. It reflects my
+          ability to solve complex problems, work with different technologies,
+          and manage projects effectively.
+        </motion.p>
+      </div>
+
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} project={project} index={index} />
+        ))}
+      </div>
+    </SectionWrapper>
   )
 }
 
-export default Works
+interface Project {
+  name: string;
+  description: string;
+  tags: {
+    name: string;
+    color: string;
+  }[];
+  image: string;
+  source_code_link: string;
+}
+
+const ProjectCard = ({ project, index }: { project: Project, index: number }) => {
+  return (
+    <motion.div variants={fadeIn('up', "spring", index * 0.5, 0.75)}>
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450
+        }}
+        className='bg-tertiary p-5 rounded-xl sm:w-[360px] w-full'
+      >
+        <div className="relative w-full h-[230px]">
+          <img
+            src={project.image}
+            alt={project.name}
+            className="w-full h-full object-cover rounded-2xl"
+          />
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div
+              onClick={() => window.open(project.source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img src={github} alt="github" className="w-1/2 h-1/2 object-contain" />
+            </div>
+          </div>
+        </div>
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{project.name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{project.description}</p>
+        </div>
+        <div className="mt-4 flex flex-wrap gap-2">
+          {project.tags.map(tag => (
+            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
+    </motion.div>
+  )
+}
+
+export default Works 
