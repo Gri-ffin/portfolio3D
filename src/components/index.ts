90022dfbdc43815f5854ff4e79b7c9e46eb7cd1f
Gri-ffin <gameshunter359@gmail.com>
2023-10-21 18:23:54 +0100

    remove feedback component import

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,7 +5,6 @@ import About from './About';
 import Tech from './Tech';
 import Experience from './Experience';
 import Works from './Works';
-import Feedbacks from './Feedbacks';
 import Contact from './Contact';
 
 export {
@@ -15,10 +14,9 @@ export {
   Tech,
   Experience,
   Works,
-  Feedbacks,
   Contact,
-  EarthCanvas, 
-  BallCanvas, 
-  ComputersCanvas, 
+  EarthCanvas,
+  BallCanvas,
+  ComputersCanvas,
   StarsCanvas
 }
