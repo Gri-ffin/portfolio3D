4ca348ad9489beb3ee972af8bea76ba687de4648
Gri-ffin <gameshunter359@gmail.com>
2024-02-19 21:49:46 +0100

    fix

--- src/hoc/SectionWrapper.tsx
+++ src/hoc/SectionWrapper.tsx
@@ -1,17 +1,21 @@
-import { motion } from "framer-motion"
-import { styles } from "../styles"
-import { staggerContainer } from "../utils/motion"
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
 
-const SectionWrapper = ({ children, idName }: { children: React.ReactNode, idName: string }) => {
+const SectionWrapper = ({
+  children,
+  idName
+}: {
+  children: React.ReactNode
+  idName: string
+}) => {
   return (
     <motion.section
-      variants={staggerContainer()}
-      initial="hidden"
-      whileInView="show"
+      initial='hidden'
+      whileInView='show'
       viewport={{ once: true, amount: 0.5 }}
       className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
     >
-      <span className="hash-span" id={idName}>
+      <span className='hash-span' id={idName}>
         &nbsp;
       </span>
 
